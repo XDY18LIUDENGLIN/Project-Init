bfe9b74633d9b9b05b2ba5d5d92c50b5299b1de3
LIUDENGLIN <1091669791@QQ.com>
2022-07-29 00:16:03 +0800

    增加: 条目信息详细查询

---
+++ src/main/java/com/liu/entity/pojo/BgmCollections.java
@@ -0,0 +1,30 @@
+package com.liu.entity.transfer;
+
+import lombok.Data;
+
+/**
+ * @author LIUDENGLIN
+ */
+@Data
+public class BgmCollections {
+    /**
+     * 在看人数
+     */
+    private Integer doing;
+    /**
+     * 搁置
+     */
+    private Integer onHold;
+    /**
+     * 抛弃
+     */
+    private Integer dropped;
+    /**
+     * 看过
+     */
+    private Integer collect;
+    /**
+     * 想看
+     */
+    private Integer wish;
+}
--- src/main/java/com/liu/entity/pojo/BgmImages.java
+++ src/main/java/com/liu/entity/pojo/BgmImages.java
@@ -3,14 +3,30 @@ package com.liu.entity.transfer;
 import lombok.Data;
 
 
-@Data
 /**
- * bgm的图片展示类
+ * @author LIUDENGLIN
+ *         bgm的图片展示类
  */
+@Data
 public class BgmImages {
+    /**
+     *
+     */
     private String large;
+    /**
+     *
+     */
     private String common;
+    /**
+     *
+     */
     private String medium;
+    /**
+     *
+     */
     private String small;
+    /**
+     *
+     */
     private String grid;
 }
